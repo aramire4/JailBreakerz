4cdec65cd318749a812382065e95f599657cc3c4
Anthony Ramirez <anthonyramirez@Anthonys-MacBook-Air.local>
2018-09-13 11:37:38 -0500

    Added dice block

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour {
+    //public int movement;
+
+    private Sprite[] diceSides;
+    private SpriteRenderer rend;
+    private int whosTurn = 1;
+    private bool coroutineAllowed = true;
+
+    // Use this for initialization
+    void Start() 
+    {
+        //movement = 0;
+        rend = GetComponent<SpriteRenderer>();
+        diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+        rend.sprite = diceSides[5];
+    }
+
+    private void OnMouseDown()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine("RollTheDice");
+        }
+    }
+
+    private IEnumerator RollTheDice()
+    {
+        coroutineAllowed = false;
+        int randomDiceSide = 0;
+        for (int i = 0; i <= 20; i++)
+        {
+            randomDiceSide = Random.Range(0, 6);
+            rend.sprite = diceSides[randomDiceSide];
+            yield return new WaitForSeconds(0.05f);
+            //movement = randomDiceSide + 1;
+            //Player moves = randomDiceSide+1;
+        }
+    }
+
+    //GameControl.diceSideThrown = randomDiceSide+1;
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float moveSize = 1.28f;
     public Vector3 pos;
+    public bool isMyTurn;
+
     private Vector3 resetPosition;
+    private int moveAmount;
     //private Rigidbody2D theRigidbody;
 
 
@@ -13,32 +16,42 @@ public class PlayerMovement : MonoBehaviour {
     void Start () {
         pos = transform.position;
         resetPosition = transform.position;
+        //moveAmount =
         //theRigidbody = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
-            pos.y += moveSize;
-            //transform.localPosition
-        }
 
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        if (isMyTurn)
         {
-            pos.y -= moveSize;
-        }
+            //if (Input.GetKeyDown("up"))
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                pos.y += moveSize;
+                //transform.localPosition
+            }
 
-        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            pos.x += moveSize;
-        }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                pos.y -= moveSize;
+            }
 
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            pos.x -= moveSize;
+            else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                pos.x += moveSize;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                pos.x -= moveSize;
+            }
+            else if(Input.GetKeyDown(KeyCode.R))
+            {
+                pos = resetPosition;
+            }
+            transform.position = pos;
         }
-        transform.position = pos;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
