5f4f500f6e40f20ee6dbd43808491f7670b26030
Anthony Ramirez <anthonyramirez@Anthonys-MacBook-Air.local>
2018-09-14 00:13:18 -0500

    Began the state machine build

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+    public State currentPlayer;
+    public int numberOfPlayers;
+
+
+    public enum State
+    {
+        Player1,
+        Player2,
+        Player3,
+        Player4,
+        Player5,
+        Player6
+    }
+
+    public void NextPlayer()
+    {
+        if (currentPlayer == State.Player1)
+        {
+            currentPlayer = State.Player2;
+        }
+        else if (currentPlayer == State.Player2)
+        {
+            if (numberOfPlayers > 2)
+            {
+                currentPlayer = State.Player3;
+            }
+            else currentPlayer = State.Player1;
+        }
+        else if (currentPlayer == State.Player3)
+        {
+            if (numberOfPlayers > 3)
+            {
+                currentPlayer = State.Player4;
+            }
+            else currentPlayer = State.Player1;
+        }
+        else if (currentPlayer == State.Player4)
+        {
+            if (numberOfPlayers > 4)
+            {
+                currentPlayer = State.Player5;
+            }
+            else currentPlayer = State.Player1;
+        }
+        else if (currentPlayer == State.Player5)
+        {
+            if (numberOfPlayers > 5)
+            {
+                currentPlayer = State.Player6;
+            }
+            else currentPlayer = State.Player1;
+        }
+        else if (currentPlayer == State.Player6)
+        {
+            currentPlayer = State.Player1;
+        }
+    }
+        // Use this for initialization
+    void Start () {
+        currentPlayer = State.Player1;
+        //TODO make this process randomized based on the number of players
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public bool active;
+    public GameState.State playerState;
+
+    private int strength;
+    private int intelligence;
+    private int looks;
+    //TODO-container of Items
+
+    //private StateMachine stateMachine = new StateMachine();
+
+
+	// Use this for initialization
+	void Start () {
+       //TODO-Instantiate PlayerState somehow
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
